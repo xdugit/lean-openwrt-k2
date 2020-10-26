5d014e92b77a855155ccfb6ea9a5eeead02ccc3f
xdugit <xdugit@users.noreply.github.com>
2020-10-26 18:08:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-
 mv -vf ../mt7620a_phicomm_psg1218.dtsi ./target/linux/ramips/dts/mt7620a_phicomm_psg1218.dtsi
 mv -vf ../mt7620.mk ./target/linux/ramips/image/mt7620.mk
+
+# Fix WiFi disabled after reboot
+sed -i "2a\ifconfig rax0 up\nifconfig ra0 up\nifconfig apclix0 up\nifconfig apcli0 up" package/base-files/files/etc/rc.local
